84786c1a348b55226320a389868f927d9d764897
Ivan Sadykov <grandpajok@gmail.com>
2020-09-14 00:42:57 +0600

    feat: create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+/**
+ *
+ * @param {string} str
+ * @param {string[][]} bracketsConfig
+ */
+function check(str, bracketsConfig) {
+    const brackets = bracketsConfig.map((b) => b.join(""));
+    for (let i = 0; i < brackets.length; i++) {
+        if (str.includes(brackets[i])) {
+            str = str.replace(brackets[i], "");
+            i = -1;
+        }
+    }
+    return !str;
 }
+
+module.exports = check;
